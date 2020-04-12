248f0c69de1d68707ebc3e92e058b3dc20153329
Matsota Andrew <matsotaandrew@icloud.com>
2020-04-12 13:44:56 +0300

    Initial Commit

---
+++ FlorentiniSVC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlorentiniSVC
+//
+//  Created by Andrew Matsota on 12.04.2020.
+//  Copyright © 2020 Andrew Matsota. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
